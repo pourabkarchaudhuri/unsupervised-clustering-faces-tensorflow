4f6996dc9f8c48683f919a09b5999c2b85b446c3
Pourab Karchaudhuri <pourab.karchaudhuri@gmail.com>
2018-08-13 21:51:13 +0530

    Removed Keys

--- send_message.py
+++ send_message.py
@@ -2,11 +2,11 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 
-account_sid = 'ACd9635007863867a4d72b9ec6b3875931'
-auth_token = '611f48880cfbf719a945dc1673f2cde1'
-numbers = ["+919748978812", "+918878339946"]
-whatsapp_source = "+14155238886"
-sms_source = "+18123591204"
+account_sid = '*PUT TWILIO ACCOUND SID*'
+auth_token = '*PUT TWILIO AUTH TOKEN*'
+numbers = ["*PUT VERIFIED PHONE NUMBERS AS RECIPIENTS*"]
+whatsapp_source = "*WHATSAPP SOURCE NUMBER*"
+sms_source = "*SMS SOURCE NUMBER*"
 payload = 'The video(s) you have uploaded are ready to be tagged. Please go to this webapp to start tagging. https://www.google.co.in/'
 
 
